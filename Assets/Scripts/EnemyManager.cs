da699c39b0d498d756113bb6d7230f893e8fcbe3
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2022-09-13 13:34:54 -0300

    List Spawning

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,18 +4,77 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    int totalEnemies = 10;
+    int waitTime = 2;
+
+    public GameObject Enemey;
+    public GameObject movePoint;
+    GameObject[] Enemies = new GameObject[10];
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 10; i++)
-        {
-
-        }
+        SpawnEnemeis();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckEnemeis();
+    }
+
+    void SpawnEnemeis()
+    {
+        for (int i = 0; i < totalEnemies ; i++)
+        {
+            Enemies[i] = Instantiate(Enemey);
+            Enemies[i].GetComponent<EnemyMove>().movementPoint = movePoint;
+        }
+    }
+
+    void CheckEnemeis()
+    {
+        for (int i = 0; i < totalEnemies; i++) // List all enemeis
+        {
+            for (int k = 0; k < totalEnemies; k++) // Enemy being checked
+            {
+                if (i != k) // makes sure to not check against itself
+                {
+                    //Debug.Log(i + " " + k);
+                    // checks to see if the enemy needs to move away from its peers
+
+                    // X
+                    if (Enemies[i].transform.position.x <
+                        Enemies[k].transform.position.x + 2 &&
+                        Enemies[i].transform.position.x !>
+                        Enemies[k].transform.position.x + 2)
+                    {
+                        //++
+                        
+                    }
+                    else if (Enemies[i].transform.position.x >
+                        Enemies[k].transform.position.x - 2 &&
+                        Enemies[i].transform.position.x !<
+                        Enemies[k].transform.position.x - 2)
+                    {
+                        //--
+                    }
+
+                    // Z
+                    if (Enemies[i].transform.position.z >
+                        Enemies[k].transform.position.z + 2)
+                    {
+                        
+                        //++
+                    }
+                    else if (Enemies[i].transform.position.z <
+                        Enemies[k].transform.position.z - 2)
+                    {
+                        //--
+                    }
+                }
+            }
+        }
     }
 }
+
